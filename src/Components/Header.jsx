bc66042cbe648eab1872d92cedcaae73bf7d0e36
khushali77 <201801264@daiict.ac.in>
2021-03-14 16:08:12 +0530

    Added: hover effect to dropdown

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,6 @@ import Navbar from "react-bootstrap/Navbar";
 import {NavDropdown,Nav} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
-
 function Header(){
     return(
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
@@ -11,16 +10,16 @@ function Header(){
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto"></Nav>
-            <Nav>
-            <NavDropdown title="My Profile" id="collasible-nav-dropdown">
+            <Nav className="nav__right">
+            <NavDropdown title={<img src="https://img.icons8.com/wired/64/000000/gender-neutral-user--v1.png"/>} id="collasible-nav-dropdown">
                 <NavDropdown.Item href="userprofile">Profile</NavDropdown.Item>
                 <NavDropdown.Item href="orders">Orders</NavDropdown.Item>
                 <NavDropdown.Item href="contactus">Contact Us</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="settings">Settings</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="fav">Favourites</Nav.Link>
-            <Nav.Link href="mybag">My Bag</Nav.Link>
+            <Nav.Link href="fav"><img src="https://img.icons8.com/ios/50/000000/like.png"/></Nav.Link>
+            <Nav.Link href="mybag"><img src="https://img.icons8.com/ios/50/000000/shopping-bag--v3.png"/></Nav.Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
