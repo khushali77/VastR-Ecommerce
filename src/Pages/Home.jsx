79ef960c613c98fb325c718a160cf8f394577aa5
khushali77 <201801264@daiict.ac.in>
2021-03-14 16:44:55 +0530

    Added: Video playback in the hompage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Video2 from "../Components/Video2.mp4";
 
 function Home(){
     return(
-        <h2>Welcome to Home Page</h2>
+        <div className="app__home">
+        <video className="app__video" autoPlay loop muted>
+            <source src={Video2} type="video/mp4" />
+        </video>
+        </div>
     )  
 }
 
